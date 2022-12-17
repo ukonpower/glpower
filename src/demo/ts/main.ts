dd8f910abd1ef15ccb9f6903d8a83d62f1705743
ukonpower <ore.ukonpower@gmail.com>
2022-12-18 01:38:49 +0900

    fix blidge

--- src/demo/ts/main.ts
+++ src/demo/ts/main.ts
@@ -58,8 +58,6 @@ window.addEventListener( 'DOMContentLoaded', () => {
 
 	const x = new GLP.Vector( 1, 2, 3, 4 );
 	x.multiply( new GLP.Vector( 1, 2, 3, 4 ) );
-	console.log( x );
-
 
 	const canvas = document.querySelector<HTMLCanvasElement>( '#canvas' )!;
 
