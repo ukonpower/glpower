62fb72bfc270643ae46efd294d6787fbdd634891
ukonpower <ore.ukonpower@gmail.com>
2023-01-20 18:47:03 +0900

    fix fxaa

--- src/demo/ts/Scene/Factory/shaders/fxaa.fs
+++ src/demo/ts/Scene/Factory/shaders/fxaa.fs
@@ -17,23 +17,24 @@ vec4 texOffset( sampler2D tex, vec2 uv, vec2 offsetPixel, vec2 resolutionInv ) {
 
 }
 
-#define FXAA_REDUCE_MIN   ( 1.0/128.0 )
-#define FXAA_REDUCE_MUL   ( 1.0/8.0 )
-#define FXAA_SPAN_MAX     8.0
+#define FXAA_REDUCE_MIN   ( 1.0 / 128.0 )
+#define FXAA_REDUCE_MUL   ( 1.0 / 8.0 )
+#define FXAA_SPAN_MAX    8.0
 
 void main( void ) {
 
 	/*--------------------------------------------------------------------------*/
 
-    vec3 rgbNW = texOffset( sampler0, vUv, vec2( 1.0, -1.0 ), uResolutionInv ).xyz;
+    vec3 rgbNW = texOffset( sampler0, vUv, vec2( -1.0, 1.0 ), uResolutionInv ).xyz;
     vec3 rgbNE = texOffset( sampler0, vUv, vec2( 1.0, 1.0 ), uResolutionInv ).xyz;
     vec3 rgbSW = texOffset( sampler0, vUv, vec2( -1.0, -1.0 ), uResolutionInv ).xyz;
-    vec3 rgbSE = texOffset( sampler0, vUv, vec2( -1.0, 1.0 ), uResolutionInv ).xyz;
+    vec3 rgbSE = texOffset( sampler0, vUv, vec2( 1.0, -1.0 ), uResolutionInv ).xyz;
     vec3 rgbM  = texture( sampler0, vUv ).xyz;
 	
 	/*--------------------------------------------------------------------------*/
 
     vec3 luma = vec3( 0.299, 0.587, 0.114 );
+
     float lumaNW = dot( rgbNW, luma );
     float lumaNE = dot( rgbNE, luma );
     float lumaSW = dot( rgbSW, luma );
@@ -54,29 +55,31 @@ void main( void ) {
 	/*--------------------------------------------------------------------------*/
 
     float dirReduce = max( ( lumaNW + lumaNE + lumaSW + lumaSE ) * ( 0.25 * FXAA_REDUCE_MUL ), FXAA_REDUCE_MIN );
-    float rcpDirMin = 1.0/( min( abs( dir.x ), abs( dir.y ) ) + dirReduce );
+    float rcpDirMin = 1.0 / ( min( abs( dir.x ), abs( dir.y ) ) + dirReduce );
     dir = min( vec2( FXAA_SPAN_MAX,  FXAA_SPAN_MAX ), max( vec2( -FXAA_SPAN_MAX, -FXAA_SPAN_MAX ), dir * rcpDirMin ) ) * uResolutionInv.xy;
 
 	/*--------------------------------------------------------------------------*/
 	
-    vec3 rgbA = ( 1.0/2.0 ) * ( 
-        texture( sampler0, vUv + dir * ( 1.0/3.0 - 0.5 ) ).xyz +
-        texture( sampler0, vUv + dir * ( 2.0/3.0 - 0.5 ) ).xyz );
-		
-    vec3 rgbB = rgbA * ( 1.0/2.0 ) + ( 1.0/4.0 ) * ( 
-        texture( sampler0, vUv + dir * ( 0.0/3.0 - 0.5 ) ).xyz +
-        texture( sampler0, vUv + dir * ( 3.0/3.0 - 0.5 ) ).xyz );
+    vec3 rgbA = ( 1.0 / 2.0 ) * ( 
+        texture( sampler0, vUv + dir * ( 1.0 / 3.0 - 0.5 ) ).xyz +
+        texture( sampler0, vUv + dir * ( 2.0 / 3.0 - 0.5 ) ).xyz
+    );
+
+    vec3 rgbB = rgbA * 0.5  + 0.25  * ( 
+        texture( sampler0, vUv + dir * -0.5 ).xyz +
+        texture( sampler0, vUv + dir *  0.5 ).xyz 
+    );
 		
     float lumaB = dot( rgbB, luma );
 
     if( ( lumaB < lumaMin ) || ( lumaB > lumaMax ) ) {
 
 		outColor = vec4( rgbA, 1.0 );
-		
+
 	} else {
 
 		outColor = vec4( rgbB, 1.0 );
 
 	};
-	
+
 }
