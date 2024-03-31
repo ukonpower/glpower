38d1af64f7e77b4951046507fbdb58ebe7056eb3
ukonpower <ore.ukonpower@gmail.com>
2024-03-31 11:47:49 +0900

    power

--- packages/glpower/src/Power.ts
+++ packages/glpower/src/Power.ts
@@ -1,8 +1,3 @@
-import { GLPowerProgram } from "./GLPowerProgram";
-import { GLPowerBuffer } from "./GLPowerBuffer";
-import { GLPowerTexture } from "./GLPowerTexture";
-import { GLPowerFrameBuffer } from "./GLPowerFrameBuffer";
-
 export class Power {
 
 	public gl: WebGL2RenderingContext;
@@ -15,41 +10,11 @@ export class Power {
 
 		this.gl.pixelStorei( gl.UNPACK_FLIP_Y_WEBGL, true );
 
+		// ext
+
 		this.gl.getExtension( "EXT_color_buffer_float" );
 		this.gl.getExtension( "EXT_color_buffer_half_float" );
 		this.extDisJointTimerQuery = this.gl.getExtension( "EXT_disjoint_timer_query_webgl2" );
-
-	}
-
-	public createProgram() {
-
-		const program = new GLPowerProgram( this.gl );
-
-		return program;
-
-	}
-
-	public createBuffer() {
-
-		const vertexBuffer = new GLPowerBuffer( this.gl );
-
-		return vertexBuffer;
-
-	}
-
-	public createTexture() {
-
-		const texture = new GLPowerTexture( this.gl );
-
-		return texture;
-
-	}
-
-	public createFrameBuffer() {
-
-		const frameBuffer = new GLPowerFrameBuffer( this.gl );
-
-		return frameBuffer;
 
 	}
 
