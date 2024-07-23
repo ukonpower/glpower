a534cd44f202c4168770cc51c40af5ab3250d1d6
ukonpower <ore.ukonpower@gmail.com>
2024-07-23 23:55:23 +0900

    wip

--- packages/glpower/src/Math/Utils.ts
+++ packages/glpower/src/Math/Utils.ts
@@ -1,6 +1,31 @@
 import { Vector } from "..";
 
-export namespace Maths {
+export namespace MathUtils {
+
+	export const gauss = ( x: number, x0: number, sx: number ) => {
+
+		let arg = x - x0;
+		arg = - 1. / 2. * arg * arg / sx;
+
+		const a = 1. / ( Math.sqrt( 2. * 3.1415 * sx ) );
+
+		return a * Math.exp( arg );
+
+	};
+
+	export const gaussWeights = ( length: number ) => {
+
+		return new Array( length ).fill( 0 ).map( ( _, i ) => {
+
+			if ( length == 0 ) return 1;
+
+			const w = ( i - 1 ) / length;
+
+			return gauss( w, 0.0, 0.5 );
+
+		} );
+
+	};
 
 	export const randomSeed = ( s: number ) => {
 
