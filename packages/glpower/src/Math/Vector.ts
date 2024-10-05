bba67feaf954830232a02030e27fbf190037e3d8
ukonpower <ore.ukonpower@gmail.com>
2024-10-05 15:30:25 +0900

    distance

--- packages/glpower/src/Math/Vector.ts
+++ packages/glpower/src/Math/Vector.ts
@@ -168,6 +168,16 @@ export class Vector {
 
 	}
 
+	public distanceTo( v: Vector | IVector3 ) {
+
+		const dx = this.x - v.x;
+		const dy = this.y - v.y;
+		const dz = this.z - v.z;
+
+		return Math.sqrt( dx * dx + dy * dy + dz * dz );
+
+	}
+
 	public normalize() {
 
 		const length = this.length() || 1;
